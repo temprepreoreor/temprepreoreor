78f9fa8b029b75780ebdcf9b054553e5b7ade5f0
temprepreoreor <91982573+temprepreoreor@users.noreply.github.com>
2021-10-10 06:28:43 +0900

    Update out.js

--- out.js
+++ out.js
@@ -1,5 +1,5 @@
 function bru() {
- var cookies = document.getElementById('f').contentDocument.cookie;
+ var cookies = document.getElementById('f').cookie;
  alert(cookies);
 };
 document.write('<img id="f" onerror="bru()" src="/user/1234" >');
